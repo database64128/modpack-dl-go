140b4b67825b0257ca5a9c3f67f8561f8e23c0c5
database64128 <free122448@hotmail.com>
2023-11-18 19:41:02 +0800

    💌 cmd: implement main routine

--- README.md
+++ README.md
@@ -6,6 +6,34 @@
 
 ⚒️⏬ Minecraft modpack downloader written in Go.
 
+## Usage
+
+```bash
+# Display help.
+modpack-dl-go -h
+
+# Retrieve and print information about a modpack and its latest version.
+modpack-dl-go -modpackID 120
+
+# Retrieve and print information about a modpack and the specified version.
+modpack-dl-go -modpackID 120 -versionID 11334
+
+# Download the latest modpack client to the specified directory.
+modpack-dl-go -modpackID 120 -clientPath /tmp/modpack-dl-go/client
+
+# Download the latest modpack server to the specified directory.
+modpack-dl-go -modpackID 120 -serverPath /tmp/modpack-dl-go/server
+
+# Download the latest modpack client and server to the specified directories.
+modpack-dl-go -modpackID 120 -clientPath /tmp/modpack-dl-go/client -serverPath /tmp/modpack-dl-go/server
+
+# Upgrade an existing modpack installation to the latest version.
+modpack-dl-go -modpackID 120 -clientPath /tmp/modpack-dl-go/client -serverPath /tmp/modpack-dl-go/server -migrateFromPath /tmp/modpack-dl-go/old
+
+# Same as above, but copy files instead of moving them.
+modpack-dl-go -modpackID 120 -clientPath /tmp/modpack-dl-go/client -serverPath /tmp/modpack-dl-go/server -migrateFromPath /tmp/modpack-dl-go/old -preserveMigrationSource
+```
+
 ## License
 
 [GPLv3](LICENSE)
--- cmd/modpack-dl-go/main.go
+++ cmd/modpack-dl-go/main.go
@@ -1,5 +1,124 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/database64128/modpack-dl-go/download"
+	"github.com/database64128/modpack-dl-go/modpacksch"
+	"github.com/database64128/modpack-dl-go/precheck"
+)
+
+var (
+	modpackID               = flag.Int64("modpackID", 0, "ID of the modpack to download")
+	versionID               = flag.Int64("versionID", 0, "Optional. Download the specified version of the modpack, instead of the latest version")
+	clientPath              = flag.String("clientPath", "", "Optional. Download the modpack client to the specified path")
+	serverPath              = flag.String("serverPath", "", "Optional. Download the modpack server to the specified path")
+	migrateFromPath         = flag.String("migrateFromPath", "", "Optional. Migrate the modpack from the specified path")
+	preserveMigrationSource = flag.Bool("preserveMigrationSource", false, "Migrate by copying instead of moving files")
+	downloadConcurrency     = flag.Int("downloadConcurrency", 32, "Optional. Number of concurrent downloads")
+)
+
+func main() {
+	flag.Parse()
+
+	if *modpackID == 0 {
+		fmt.Println("Please specify a modpack ID with '-modpackID'.")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	logger := slog.Default()
+	ctx, cancel := context.WithCancel(context.Background())
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		sig := <-sigCh
+		logger.LogAttrs(ctx, slog.LevelInfo, "Received exit signal", slog.Any("signal", sig))
+		cancel()
+	}()
+
+	modpackManifest, err := modpacksch.GetPublicModpackManifest(ctx, *modpackID)
+	if err != nil {
+		logger.LogAttrs(ctx, slog.LevelError, "Failed to get modpack manifest",
+			slog.Int64("modpackID", *modpackID),
+			slog.Any("error", err),
+		)
+		os.Exit(1)
+	}
+
+	logger.LogAttrs(ctx, slog.LevelInfo, "Got modpack manifest",
+		slog.Int64("modpackID", modpackManifest.ID),
+		slog.String("name", modpackManifest.Name),
+		slog.String("synopsis", modpackManifest.Synopsis),
+		slog.Any("versions", modpackManifest.Versions),
+	)
+
+	if *versionID == 0 {
+		version, ok := modpackManifest.LatestVersion()
+		if !ok {
+			logger.LogAttrs(ctx, slog.LevelError, "Modpack has no versions")
+			os.Exit(1)
+		}
+		*versionID = version.ID
+	}
+
+	versionManifest, err := modpacksch.GetPublicModpackVersionManifest(ctx, *modpackID, *versionID)
+	if err != nil {
+		logger.LogAttrs(ctx, slog.LevelError, "Failed to get modpack version manifest",
+			slog.Int64("modpackID", *modpackID),
+			slog.Int64("versionID", *versionID),
+			slog.Any("error", err),
+		)
+		os.Exit(1)
+	}
+
+	logger.LogAttrs(ctx, slog.LevelInfo, "Got modpack version manifest",
+		slog.Int64("modpackID", versionManifest.Parent),
+		slog.Int64("versionID", versionManifest.ID),
+		slog.String("name", versionManifest.Name),
+		slog.String("type", versionManifest.Type),
+		slog.Time("updated", time.Time(versionManifest.Updated)),
+		slog.Int("fileCount", len(versionManifest.Files)),
+	)
+
+	if *clientPath == "" && *serverPath == "" {
+		logger.LogAttrs(ctx, slog.LevelInfo, "User did not ask to download anything")
+		return
+	}
+
+	pjch := make(chan precheck.Job)
+	pwf := precheck.NewWorkerFleet(ctx, logger, pjch)
+	dwf := download.NewWorkerFleet(ctx, logger, http.DefaultClient, *downloadConcurrency, pwf.DownloadJobChannel())
+
+	for i := range versionManifest.Files {
+		file := &versionManifest.Files[i]
+		pj, ok, err := file.PrecheckJob(*migrateFromPath, *clientPath, *serverPath, *preserveMigrationSource)
+		if err != nil {
+			logger.LogAttrs(ctx, slog.LevelWarn, "Failed to create precheck job",
+				slog.Int64("modpackID", versionManifest.Parent),
+				slog.Int64("versionID", versionManifest.ID),
+				slog.String("name", file.Name),
+				slog.String("path", file.Path),
+				slog.Any("error", err),
+			)
+			continue
+		}
+		if !ok {
+			continue
+		}
+		pjch <- pj
+	}
+
+	close(pjch)
+	pwf.Wait()
+	dwf.Wait()
 }
